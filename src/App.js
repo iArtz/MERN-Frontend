272c4a2c7c7ded0baa6d8a4fce13c16ac31e216b
iArtz <dev@iartz.io>
2020-02-22 17:36:07 +0700

    49. Rendering User Places & Using Dynamic Route Segments

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import {
 import Users from './user/pages/Users';
 import NewPlace from './places/pages/NewPlace';
 import MainNavigation from './shared/components/Navigation/MainNavigation';
+import UserPlaces from './places/pages/UserPlaces';
 
 function App() {
   return (
@@ -15,6 +16,9 @@ function App() {
         <Switch>
           <Route path="/" exact>
             <Users />
+          </Route>
+          <Route path="/:userId/places" exact>
+            <UserPlaces />
           </Route>
           <Route path="/places/new" exact>
             <NewPlace />
