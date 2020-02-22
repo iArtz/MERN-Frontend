1b3923e02baab951caadd09273c101f2351a9a32
iArtz <dev@iartz.io>
2020-02-22 12:39:55 +0700

    38. Starting Setup, Pages & Routes

---
+++ src/places/pages/NewPlace.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const NewPlace = () => <h2>NewPlace Works!</h2>;
+
+export default NewPlace;
---
+++ src/user/pages/Users.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const Users = () => <h2>Users Works!</h2>;
+
+export default Users;
