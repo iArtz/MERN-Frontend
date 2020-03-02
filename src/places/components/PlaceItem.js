1d1ce7f214b0bd6c088bd69537870f0e8bb7fc63
iArtz <dev@iartz.io>
2020-03-02 08:48:56 +0700

    74. More Auth Context Usage

--- src/places/components/PlaceItem.js
+++ src/places/components/PlaceItem.js
@@ -1,14 +1,17 @@
-import React, { useState } from 'react';
+import React, { useState, useContext } from 'react';
 
 import Card from '../../shared/components/UIElements/Card';
 import Button from '../../shared/components/FormElements/Button';
 import Modal from '../../shared/components/UIElements/Modal';
 import Map from '../../shared/components/UIElements/Map';
+import { AuthContext } from '../../shared/context/auth-context';
 import './PlaceItem.css';
 
 const PlaceItem = ({
   id, title, address, description, image, coordinates,
 }) => {
+  const auth = useContext(AuthContext);
+
   const [showMap, setShowMap] = useState(false);
 
   const [showConfirmModal, setShowConfirm] = useState(false);
@@ -78,10 +81,12 @@ const PlaceItem = ({
               <Button inverse onClick={openMapHandler}>
                 VIEW ON MAP
               </Button>
-              <Button to={`/places/${id}`}>EDIT</Button>
-              <Button danger onClick={showDeleteWarningHandler}>
-                DELETE
-              </Button>
+              {auth.isLoggedIn && <Button to={`/places/${id}`}>EDIT</Button>}
+              {auth.isLoggedIn && (
+                <Button danger onClick={showDeleteWarningHandler}>
+                  DELETE
+                </Button>
+              )}
             </div>
           </div>
         </Card>
